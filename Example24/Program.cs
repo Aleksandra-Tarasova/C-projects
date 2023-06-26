65e646af85be91c51f64a60829766aea727ec1e8
Aleksandra Tarasova <tarasandra0910@gmail.com>
2023-06-27 00:17:01 +0700

    Added folder Example24

---
+++ Example24/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int A = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(A);
+Console.WriteLine($"{A} -> {sumNumbers}");
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum; 
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write("Введите целое число A: ");
+        if(int.TryParse(Console.ReadLine(), out int userNumber))
+        return userNumber;
+
+        Console.WriteLine(errorMessage);
+    }
+}
