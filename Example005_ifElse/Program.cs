f16be2635f5a57835d9c5d61d7d5b4d867f661d0
Aleksandra Tarasova <tarasandra0910@gmail.com>
2023-08-08 20:11:35 +0700

    Added Example005 If else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "sasha")
+{
+    Console.WriteLine("Господи! Какое счастье! Это же Саша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
